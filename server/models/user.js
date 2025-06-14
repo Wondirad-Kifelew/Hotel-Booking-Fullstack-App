83c534c3a8436d759ddbbc8630c3df09f6581c7c
Wondirad-Kifelew <wondiradabebekifelew@gmail.com>
2025-06-14 16:17:25 +0300

    d

--- server/models/user.js
+++ server/models/user.js
@@ -6,6 +6,8 @@ const userSchema = mongoose.Schema({
     email: {type: String, required: true},
     role: {type: String, enum: ["user", "hotelOwner"], default: "user"},
     recentSearchCities: [{type: String, required: true}],
+    // "s":Number,
+    // "r":Number
 } , {timestamps: true})
 
 const User = mongoose.model("User", userSchema)
--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 import "dotenv/config"
 import express from 'express'
 import cors from "cors"
-import connectDb from './configs/db.js'
+// import connectDb from './configs/db.js'
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebHooks from "./controllers/clerkwebhooks.js"
+import User from './models/user.js'
+import mongoose from 'mongoose'
+
+const connectDb = async ()=>{
+   try {
+    mongoose.connection.on('connected', ()=>console.log("database connected")   )
+    await mongoose.connect(process.env.MONGODB_URI)
+    
+} catch (error) {
+    console.log("error connecting to db")
+    console.log("Error: ", error.message)
+}}
 
 connectDb()
+
 const app = express()
 
 app.use(cors())
@@ -18,6 +31,11 @@ app.use(clerkMiddleware())
 app.post('/api/clerk', clerkWebHooks) 
 app.get('/', (req, res)=>res.send("API is working!"))
 
+app.post('/api/test', async (req, res)=>{
+console.log("in test post", req.body)
+const c = req.body
+await User.create(c)
+})
 
 const port = process.env.PORT || 5000
 
