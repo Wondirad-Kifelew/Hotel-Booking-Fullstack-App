8ba854b11c09afe5228b49b6ec9b4303bd759ad2
Wondirad-Kifelew <wondiradabebekifelew@gmail.com>
2025-06-14 14:08:43 +0300

    modified mongodb uri

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDb = async ()=>{
    try {
     mongoose.connection.on('connected', ()=>console.log("database connected")   )
-    await mongoose.connect(`${process.env.MONGODB_URI}/hotel_booking_app`)
+    await mongoose.connect(process.env.MONGODB_URI)
     
 } catch (error) {
     console.log("error connecting to db")
